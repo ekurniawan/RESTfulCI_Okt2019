58794e1d2ee11c779b2e00ce8ea0bf3590846fcf
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-10-30 13:51:20 +0700

    menambahkan post

--- application/controllers/api/Mahasiswa.php
+++ application/controllers/api/Mahasiswa.php
@@ -1,14 +1,14 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 require APPPATH . 'libraries/REST_Controller.php';
 
-class Mahasiswa extends REST_Controller {
+class Mahasiswa extends REST_Controller
+{
 
     function __construct()
     {
         // Construct the parent class
         parent::__construct();
-
     }
 
     //http://localhost/samplerestapi/index.php/api/mahasiswa
@@ -25,29 +25,31 @@ class Mahasiswa extends REST_Controller {
         }
         return $this->response($data,200);
     }*/
-    
-    public function index_get(){
-        $this->db->select('nim,nama');
+
+    public function index_get()
+    {
+        //$this->db->select('nim,nama');
         $data = $this->db->get('mahasiswa')->result();
-        return $this->response($data,200);
+        return $this->response($data, 200);
     }
 
-    public function getbyid_get($nim){
-        $this->db->where('nim',$nim);
+    public function getbyid_get($nim)
+    {
+        $this->db->where('nim', $nim);
         $data = $this->db->get('mahasiswa')->result();
-        if(count($data)==0){
-            return $this->response("Data $nim tidak ditemukan ",400); 
-        }
-        else {
-            return $this->response($data[0],200); 
+        if (count($data) == 0) {
+            return $this->response("Data $nim tidak ditemukan ", 400);
+        } else {
+            return $this->response($data[0], 200);
         }
     }
 
     //https://codeigniter.com/userguide2/database/active_record.html#select
-    public function getbynama_get($nama){
-        $this->db->like('nama',$nama);
+    public function getbynama_get($nama)
+    {
+        $this->db->like('nama', $nama);
         $data = $this->db->get('mahasiswa')->result();
-        return $this->response($data,200);
+        return $this->response($data, 200);
     }
 
     //http://localhost/samplerestapi/index.php/api/mahasiswa/getbyid/72006666
@@ -62,33 +64,64 @@ class Mahasiswa extends REST_Controller {
         }
     }*/
 
-    public function tampil_get(){
-        $arrNama = array(array("id"=>"12345","nama"=>"budi","alamat"=>"pekanbaru"),
-        array("id"=>"12389","nama"=>"erick","alamat"=>"jogja"));
-        return $this->response($arrNama,200);
+    public function tampil_get()
+    {
+        $arrNama = array(
+            array("id" => "12345", "nama" => "budi", "alamat" => "pekanbaru"),
+            array("id" => "12389", "nama" => "erick", "alamat" => "jogja")
+        );
+        return $this->response($arrNama, 200);
     }
+
+    /*public function index_post()
+    {
+        $nim = $this->post('nim');
+        $nama = $this->post('nama');
+        $email = $this->post('email');
+        $ipk = $this->post('ipk');
+
+        $sql = "insert into mahasiswa(nim,nama,email,ipk) values(?,?,?,?)";
+        $result = $this->db->query($sql,array($nim,$nama,$email,$ipk));
+        if($result!=1){
+            return $this->response("Gagal menambah data",400);
+        }
+        else {
+            return $this->response("Data berhasil ditambah",200);
+        }
+    }*/
 
     public function index_post(){
         $nim = $this->post('nim');
         $nama = $this->post('nama');
+        $email = $this->post('email');
+        $ipk = $this->post('ipk');
 
-        $data = array('nim'=>$nim,'nama'=>$nama);
-
-        return $this->response($data,200);
+        $data = array("nim"=>$nim,"nama"=>$nama,"email"=>$email,"ipk"=>$ipk);
+        $result = $this->db->insert('mahasiswa',$data);
+        if($result!=1){
+            return $this->response("Gagal menambah data",400);
+        }
+        else {
+            return $this->response(array("status"=>"201","data"=>$data),201);
+        }
     }
 
-    public function index_put(){
+    /*public function index_put(){
+
+    }*/
+
+    public function index_put()
+    {
         $nim = $this->put('nim');
         $nama = $this->put('nama');
 
-        $data = "Data ".$nim." dan ".$nama." berhasil di edit ";
-        return $this->response($data,200);
+        $data = "Data " . $nim . " dan " . $nama . " berhasil di edit ";
+        return $this->response($data, 200);
     }
 
-    public function index_delete($nim){
-        $data = "Data nim ".$nim." berhasil didelete";
-        return $this->response($data,200);
+    public function index_delete($nim)
+    {
+        $data = "Data nim " . $nim . " berhasil didelete";
+        return $this->response($data, 200);
     }
 }
-
-?>
